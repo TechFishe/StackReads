b29b897aeeef3d6bf59a9d088d480059406a3100
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-29 15:13:44 -0600

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="flex h-20 w-full items-center justify-between border-b-4 border-snow/15">
+    <header class="flex h-20 w-full items-center justify-between border-b-4 border-snow/15 py-1 px-2">
       <section class="flex items-center">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-16 w-16">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
