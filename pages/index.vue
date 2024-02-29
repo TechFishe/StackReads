ab163e9f72d6b7b1ca216c37fefeb1a523b84fd6
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-29 13:09:46 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,5 @@
 </script>
 
 <template>
-  <main id="backgroundArt" class="flex h-fullscreen w-screen flex-col"></main>
+  <main id="bgArt" class="flex h-fullscreen w-screen flex-col"></main>
 </template>
