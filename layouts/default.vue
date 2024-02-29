467dd2bc1b63242a29195dbee34b54471972e115
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-29 15:04:44 -0600

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
         </svg>
         <div class="mx-1 h-14 w-[2px] rounded-full bg-snow/45"></div>
-        <span class="flex font-handlee text-7xl font-bold tracking-wider text-tiff-300 subpixel-antialiased">StackReads</span>
+        <span class="flex font-handlee text-6xl font-bold tracking-wider text-tiff-300 subpixel-antialiased">StackReads</span>
       </section>
       <nav class="flex items-center space-x-6">
         <NavBtn url="/" icon="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
