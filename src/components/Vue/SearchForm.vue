f7483c6fa4543e72c623bad391821057ca48c73b
TechFishe <jacobb2008@icloud.com>
2024-06-24 11:59:44 -0500

    Update SearchForm.vue

--- src/components/Vue/SearchForm.vue
+++ src/components/Vue/SearchForm.vue
@@ -32,7 +32,6 @@
     }
 
     query += `&printType=books&startIndex=${startIndex}&maxResults=40&key=${import.meta.env.PUBLIC_BOOK_API}`;
-    console.log(query);
 
     const response = await fetch(query);
     const data = await response.json();
@@ -49,12 +48,12 @@
 
 <template>
   <div class="flex h-3/4 w-5/6 flex-col items-center justify-center space-y-12">
-    <article class="flex w-5/12 flex-col items-center justify-center space-y-4 rounded-md bg-Woodsmoke-900/60 px-4 py-1">
+    <article class="flex w-5/6 flex-col items-center justify-center space-y-4 rounded-md bg-Woodsmoke-900/60 px-4 py-1 lg:w-3/4 xl:w-2/3 2xl:w-1/2">
       <h1 class="w-full border-b border-b-SpringWood-50/25 pb-1 text-center font-Playpen text-5xl font-light">Search</h1>
       <form @submit.prevent="queryBooks" method="get" class="flex w-full flex-col items-center justify-center space-y-4 pb-1">
-        <section class="flex w-full items-center justify-around">
-          <input v-model="title" type="text" autocomplete="off" name="title" id="title" placeholder="Title" class="w-5/12 rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
-          <input v-model="author" type="text" autocomplete="off" name="author" id="author" placeholder="Author" class="w-5/12 rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
+        <section class="flex w-full flex-col items-center space-y-4 px-4 md:flex-row md:space-x-4 md:space-y-0 lg:px-8">
+          <input v-model="title" type="text" autocomplete="off" name="title" id="title" placeholder="Title" class="w-full rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
+          <input v-model="author" type="text" autocomplete="off" name="author" id="author" placeholder="Author" class="w-full rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
         </section>
         <span v-if="error != ''" class="font-medium text-red-500">{{ error }}</span>
         <button type="submit" class="rounded-md border px-4 py-1 text-xl shadow-md shadow-transparent transition-all ease-out hover:border-green-500 hover:text-green-500 hover:shadow-green-300/30">Submit</button>
