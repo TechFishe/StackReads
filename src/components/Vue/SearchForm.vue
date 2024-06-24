e0c5410a05726ebcede802a079a2099b319512a7
TechFishe <jacobb2008@icloud.com>
2024-06-24 17:28:39 -0500

    More mobile support!

--- src/components/Vue/SearchForm.vue
+++ src/components/Vue/SearchForm.vue
@@ -47,20 +47,22 @@
 </script>
 
 <template>
-  <div class="flex h-3/4 w-5/6 flex-col items-center justify-center space-y-12">
-    <article class="flex w-full flex-col items-center justify-center space-y-4 rounded-md bg-Woodsmoke-900/60 px-4 py-1 md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-5/12">
-      <h1 class="w-full border-b border-b-SpringWood-50/25 pb-1 text-center font-Playpen text-5xl font-light">Search</h1>
-      <form @submit.prevent="queryBooks" method="get" class="flex w-full flex-col items-center justify-center space-y-4 pb-1">
-        <section class="flex w-full flex-col items-center space-y-4 px-4 md:flex-row md:space-x-4 md:space-y-0 lg:px-8">
-          <input v-model="title" type="text" autocomplete="off" name="title" id="title" placeholder="Title" class="w-full rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
-          <input v-model="author" type="text" autocomplete="off" name="author" id="author" placeholder="Author" class="w-full rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
-        </section>
-        <span v-if="error != ''" class="font-medium text-red-500">{{ error }}</span>
-        <button type="submit" class="rounded-md border px-4 py-1 text-xl shadow-md shadow-transparent transition-all ease-out hover:border-green-500 hover:text-green-500 hover:shadow-green-300/30">Submit</button>
-      </form>
-    </article>
-    <article class="w-full">
-      <ul class="flex w-full items-center justify-between">
+  <div class="flex h-3/4 w-full flex-col items-center justify-center space-y-8">
+    <div class="flex w-5/6 items-center justify-center">
+      <article class="flex w-full flex-col items-center justify-center space-y-4 rounded-md bg-Woodsmoke-900/60 px-4 py-1 md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-5/12">
+        <h1 class="w-full border-b border-b-SpringWood-50/25 pb-1 text-center font-Playpen text-5xl font-light">Search</h1>
+        <form @submit.prevent="queryBooks" method="get" class="flex w-full flex-col items-center justify-center space-y-4 pb-1">
+          <section class="flex w-full flex-col items-center space-y-4 px-4 md:flex-row md:space-x-4 md:space-y-0 lg:px-8">
+            <input v-model="title" type="text" autocomplete="off" name="title" id="title" placeholder="Title" class="w-full rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
+            <input v-model="author" type="text" autocomplete="off" name="author" id="author" placeholder="Author" class="w-full rounded-md bg-Woodsmoke-900/80 px-2 py-0.5 text-lg outline-0 placeholder:text-base placeholder:italic" />
+          </section>
+          <span v-if="error != ''" class="font-medium text-red-500">{{ error }}</span>
+          <button type="submit" class="rounded-md border px-4 py-1 text-xl shadow-md shadow-transparent transition-all ease-out hover:border-green-500 hover:text-green-500 hover:shadow-green-300/30">Submit</button>
+        </form>
+      </article>
+    </div>
+    <article class="w-11/12">
+      <ul class="grid w-full grid-cols-1 grid-rows-10 items-end justify-evenly justify-items-center space-y-6 md:grid-cols-5 md:grid-rows-2 2xl:grid-cols-10 2xl:grid-rows-1 2xl:space-y-0">
         <li v-for="book in currBooks" class="group h-52 bg-Woodsmoke-950 shadow-lg shadow-transparent transition-all ease-out hover:scale-105 hover:opacity-80 hover:shadow-MonteCarlo-300/40">
           <a :href="props.url + `/books/${book.id}`" class="transition-opacity ease-out group-hover:opacity-90">
             <img :src="book.volumeInfo.imageLinks.thumbnail" alt="Book cover" class="h-full rounded-sm" />
