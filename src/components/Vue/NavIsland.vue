ef5a9068bee1588ec831a24fff959c9a1d7a9f4f
TechFishe <jacobb2008@icloud.com>
2024-07-06 17:29:23 -0500

    New nav island!

---
+++ src/components/Vue/NavIsland.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import { onMounted, onUnmounted, ref } from 'vue';
+
+  const showNav = ref(true);
+  let prevPos = 0;
+
+  function onScoll() {
+    const currPos = window.scrollY;
+
+    if (prevPos < currPos) {
+      showNav.value = false;
+    } else if (prevPos > currPos) {
+      showNav.value = true;
+    }
+
+    prevPos = currPos;
+  }
+
+  onMounted(() => {
+    window.addEventListener('scroll', onScoll);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('scroll', onScoll);
+  });
+</script>
+
+<template>
+  <Transition name="fade">
+    <nav v-if="showNav" class="fixed bottom-0 left-1/2 right-1/2 mb-4 flex w-fit -translate-x-1/2 items-center justify-center space-x-6 rounded-lg bg-Woodsmoke-900/80 px-8 py-1 sm:space-x-8 sm:px-12 md:hidden">
+      <slot />
+    </nav>
+  </Transition>
+</template>
+
+<style>
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-to,
+  .fade-leave-from {
+    opacity: 100;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition-property: opacity;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 200ms;
+  }
+</style>
